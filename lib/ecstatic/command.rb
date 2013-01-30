551777c6b9e519c0aa57a8246651920dd0ea2a5d
Matt Gauger <matt.gauger@gmail.com>
2013-01-29 23:17:26 -0600

    If this is a Github Pages site, set up a gh-pages dir
    - Empty directory will get a remote to the same repo.
    - Pushing to the gh-pages branch will deploy the site.

--- lib/ecstatic/command.rb
+++ lib/ecstatic/command.rb
@@ -4,19 +4,37 @@ module Ecstatic
     include Ecstatic::Actions
 
     def self.source_root
-      File.expand_path("../templates", __FILE__)
+      File.expand_path('../templates', __FILE__)
     end
 
-    desc "init", "Create a new ecstatic project"
+    desc 'init', 'Create a new ecstatic project'
+    method_options ghpages: false, aliases: '-gh', banner: "test"
     def init
-      git :init
+      init_git_if_not_already
       directory 'site'
       copy_file '.gitignore'
+
+      if options.ghpages?
+        empty_directory 'gh-pages'
+        init_git_if_not_already('gh-pages')
+        Dir.chdir('gh-pages') do
+          git :checkout => '-b gh-pages'
+        end
+      end
     end
 
-    desc "server", "Run your site locally, recompiling files as they change."
+    desc 'server', 'Run your site locally, recompiling files as they change.'
     def server
       Ecstatic::Server.run!
+    end
+
+    private
+    def init_git_if_not_already(path = '.')
+      unless Dir.exists?(File.join(path, '.git'))
+        git :init => path
+      else
+        puts "Skipping git init because #{path} already has a .git/"
+      end
     end
   end
 end
