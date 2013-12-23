0eda18a6cdfc91b3c61e2cbbaf11700fd4133809
Matt Gauger <matt.gauger@gmail.com>
2013-12-22 21:24:19 -0600

    Add CoffeeScript support

--- lib/ecstatic/watcher.rb
+++ lib/ecstatic/watcher.rb
@@ -4,12 +4,21 @@ module Ecstatic
   class Watcher
     def self.watch!
       script = Watchr::Script.new
+
+      # haml
       script.watch('site/pages/(.*)\.haml') do |m|
         puts "Compiling page: #{m[0]}"
         system("haml site/pages/#{m[1]}.haml gh-pages/#{m[1]}.html")
       end
 
+      # scss
       script.watch('site/assets/stylesheets/.*') { puts 'Compiling scss'; system('compass compile') }
+
+      # coffeescript
+      script.watch('site/scripts/(.*)\.coffee') do |m|
+        puts "Compiling CoffeeScript file: #{m[0]}"
+        system("coffee -o gh-pages/javascripts -c site/scripts/#{m[1]}.coffee")
+      end
 
       handler = Watchr.handler.new
 
